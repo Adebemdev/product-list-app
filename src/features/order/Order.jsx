e8dc06c6fc58f9383656179524c99b9bf2cef6c6
Adebemdev <adeolabusayo1995@gmail.com>
2024-10-05 01:20:42 -0700

    feat: project done

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -17,15 +17,15 @@ const Order = ({ cart = [], isOpen, handleOrder }) => {
           className="fixed inset-0 z-40 bg-rose900 opacity-50"
         ></div>
         <div className="fixed inset-0 z-50 flex h-screen w-screen items-center justify-center">
-          <div className="mt-20 min-h-80 w-full max-w-full rounded-lg bg-rose50 p-8 shadow-lg laptop:col-start-2 laptop:col-end-3 laptop:mt-0 laptop:h-auto laptop:w-full laptop:max-w-md">
+          <div className="laptop:col-start-2 laptop:col-end-3 laptop:mt-0 laptop:h-auto laptop:w-full laptop:max-w-md mt-10 min-h-full w-full max-w-full rounded-lg bg-rose50 p-8 shadow-lg">
             <div>
-              <img src={orderIcon} alt="" className="mb-2 laptop:mb-2" />
+              <img src={orderIcon} alt="" className="laptop:mb-2 mb-2" />
               <p
                 data-testid="order-confirmed"
-                className="mb-2 text-3xl font-bold capitalize tracking-normal laptop:text-3xl"
+                className="laptop:text-3xl mb-2 text-3xl font-bold capitalize tracking-normal"
               >
                 order{' '}
-                <span className="block laptop:inline-block">confrimed</span>
+                <span className="laptop:inline-block block">confrimed</span>
               </p>
               <p className="mb-4 text-base text-rose400">
                 We hope you enjoy your food!
